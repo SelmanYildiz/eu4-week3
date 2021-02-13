dedc31c81e785957370440db1aa170869cbe4b11
mustafakaralik <75313489+mustafakaralik@users.noreply.github.com>
2021-02-13 20:48:44 +0300

    Update Abdusadik_Task001.java
    update ettim

--- src/test/java/abdusadik/Abdusadik_Task001.java
+++ src/test/java/abdusadik/Abdusadik_Task001.java
@@ -4,5 +4,6 @@ public class Abdusadik_Task001 {
     public static void main(String[] args) {
 
         System.out.println("Hello World from Abdusadik`s branch");
+        Sysyem.out.println("You are the best my team member"); //Thks bro
     }
 }
